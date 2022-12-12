df1876baf9951303e7abae3454ef9dee2e40f52d
dongyoon7212 <dongyoon7212@naver.com>
2022-12-13 03:20:36 +0900

    layout 완료

--- ReminderProject/ViewController.swift
+++ ReminderProject/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myList: UILabel!
+    @IBOutlet weak var newList: UIButton!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var addList: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var option: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
---
+++ ReminderProject/collectionCellDummy.swift
@@ -0,0 +1,8 @@
+//
+//  collectionCellDummy.swift
+//  ReminderProject
+//
+//  Created by 이동윤 on 2022/12/13.
+//
+
+import UIKit
