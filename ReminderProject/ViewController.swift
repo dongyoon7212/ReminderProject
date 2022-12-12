320426af071619ac6a7fc309952f6e2ce3f8f734
dongyoon7212 <dongyoon7212@naver.com>
2022-12-13 02:13:29 +0900

    Initial Commit

---
+++ ReminderProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderProject
+//
+//  Created by 이동윤 on 2022/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
